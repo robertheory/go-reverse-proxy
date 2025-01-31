46088b8a510551c0ea619d4fe831240b1cb057ed
Roberto Costa <roberto.araujo339@gmail.com>
2025-01-30 23:11:39 -0300

    Add caching mechanism to reverse proxy for improved performance

--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
 	"io"
 	"log"
 	"math/rand"
 	"net/http"
 	"net/url"
+	"sync"
 	"time"
 )
 
+type Cache struct {
+	data map[string][]byte
+	ttl  map[string]time.Time
+	mu   sync.RWMutex
+}
+
 type ReverseProxy struct {
 	routes map[string][]string
+	cache  Cache
+}
+
+func NewCache() *Cache {
+	return &Cache{
+		data: make(map[string][]byte),
+		ttl:  make(map[string]time.Time),
+	}
 }
 
 func NewReverseProxy() *ReverseProxy {
@@ -21,7 +39,70 @@ func NewReverseProxy() *ReverseProxy {
 				"http://jsonplaceholder.typicode.com",
 			},
 		},
+		cache: *NewCache(),
 	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	if expiration, exist := c.ttl[key]; exist && time.Now().Before(expiration) {
+		return c.data[key], true
+	}
+
+	return nil, false
+}
+
+func (c *Cache) Set(key string, value []byte, ttl time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.data[key] = value
+	c.ttl[key] = time.Now().Add(ttl)
+}
+
+func (c *Cache) CleanUp() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	for k, v := range c.ttl {
+		if time.Now().After(v) {
+			delete(c.data, k)
+			delete(c.ttl, k)
+		}
+	}
+}
+
+func (rp *ReverseProxy) cacheMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		key := fmt.Sprintf("%s-%x", r.URL.Path, sha256.Sum256([]byte(r.URL.RawQuery)))
+
+		if cache, ok := rp.cache.Get(key); ok {
+			w.Write(cache)
+			fmt.Printf("Cache hit: %s\n", r.URL.Path)
+			return
+		}
+
+		recorder := &responseRecorder{
+			ResponseWriter: w,
+			body:           bytes.NewBuffer(nil),
+		}
+
+		next(recorder, r)
+
+		rp.cache.Set(key, recorder.body.Bytes(), 5*time.Minute)
+	}
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (r *responseRecorder) Write(b []byte) (int, error) {
+	r.body.Write(b)
+	return r.ResponseWriter.Write(b)
 }
 
 func (rp *ReverseProxy) selectBackend(path string) (string, bool) {
@@ -88,8 +169,9 @@ func (rp *ReverseProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	rp := NewReverseProxy()
-	http.Handle("/", rp)
+	proxy := NewReverseProxy()
+
+	http.HandleFunc("/", proxy.cacheMiddleware(proxy.ServeHTTP))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
